f500ca8fcc0dce807ce31da91400405d0d9de721
Valérie Cote <libelul2a@gmail.com>
2025-03-04 00:37:29 +0100

    Suppression des fichiers suivis indésirables

--- Backend/app.js
+++ Backend/app.js
@@ -1,27 +1,33 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors')
-require('dotenv').config();
-const helmet = require('helmet');
-const swaggerUi = require('swagger-ui-express')
-const yaml = require('yamljs')
-const swaggerDocs = yaml.load('swagger.yaml')
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(helmet({
-      crossOriginResourcePolicy: false,
-    }));
-app.use('/images', express.static(path.join(__dirname, 'images')))
-
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+require("dotenv").config();
+const helmet = require("helmet");
+const swaggerUi = require("swagger-ui-express");
+const yaml = require("yamljs");
+const swaggerDocs = yaml.load("swagger.yaml");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
+// Log de chaque requête reçue par ton serveur
+app.use((req, res, next) => {
+  console.log(`Request received: ${req.method} ${req.url}`);
+  next(); // Appeler le prochain middleware
+});
 const db = require("./models");
-const userRoutes = require('./routes/user.routes');
-const categoriesRoutes = require('./routes/categories.routes');
-const worksRoutes = require('./routes/works.routes');
-db.sequelize.sync().then(()=> console.log('db is ready'));
-app.use('/api/users', userRoutes);
-app.use('/api/categories', categoriesRoutes);
-app.use('/api/works', worksRoutes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+const userRoutes = require("./routes/user.routes");
+const categoriesRoutes = require("./routes/categories.routes");
+const worksRoutes = require("./routes/works.routes");
+db.sequelize.sync().then(() => console.log("db is ready"));
+app.use("/api/users", userRoutes);
+app.use("/api/categories", categoriesRoutes);
+app.use("/api/works", worksRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 module.exports = app;
--- Backend/models/works.model.js
+++ Backend/models/works.model.js
@@ -1,18 +1,26 @@
-
 module.exports = (sequelize, DataTypes) => {
-	const Works = sequelize.define(
-		"works",
-		{
-		title: {
-			type: DataTypes.STRING,
-			allowNull: false
-			},
-		imageUrl: {
-			type: DataTypes.STRING,
-			allowNull: false
-			}
-		},
-		{timestamps:false}
-	)
-	return Works
-}
+  const Works = sequelize.define(
+    'works',
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      imageUrl: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      categoryId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      userId: {
+        // Ajout de userId
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {timestamps: false}
+  );
+  return Works;
+};
